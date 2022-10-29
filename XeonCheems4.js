6706aa2130e100e097fbd8e37dedf5dd16805123
KamikazakiThouta <ravensvenix@gmail.com>
2022-10-30 04:00:38 +0800

    Add files via upload

--- XeonCheems4.js
+++ XeonCheems4.js
@@ -57,7 +57,7 @@ const xeontod = require("tod-api")
 const { pinterest } = require("./lib/pinterest")
 const toHur = require('@develoka/angka-terbilang-js')
 const { lolkey } = JSON.parse(fs.readFileSync('./lolkey.js'))
-const { fetchJson, getBuffer } = require('./lib/fetcher.js')
+const { getBuffer } = require('./lib/fetcher.js')
 const { hentai } = require('./lib/scraper2.js')
 const {
  FajarNews, 
