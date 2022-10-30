3ed078aceab7b8edb9bb4c6d86274a8302889412
KamikazakiThouta <ravensvenix@gmail.com>
2022-10-31 07:05:33 +0800

    Add files via upload

--- elainav3.js
+++ elainav3.js
@@ -8141,8 +8141,9 @@ if (isBan) return reply(mess.ban)
 if (isBanChat) return reply(mess.banChat)
            if (!text) return reply('niat kaga, link aja ga dikasih')
 			   reply ('Sebentar Kak, lagi loading <3')
-			   tktoknowm = await fetchJson(`https://api.lolhuman.xyz/api/tiktokwm?apikey=${lolkey}&url=${body.slice(11)}`)
-			   tktoknowm2 = await getBuffer(tktoknowm.result.body)
+		       linkurl = `https://api.lolhuman.xyz/api/tiktokwm?apikey=${lolkey}&url=${body.slice(11)}`
+			   tktoknowm = await fetchJson(linkurl)
+			   tktoknowm2 = await getBuffer(tktoknowm.result.link)
 		   XeonBotInc.sendMessage(m.chat, { video: { url: tktoknowm2 }, caption: `TikTok Download NoWM Selesai!` }, { quoted: m })
 	}
          break
