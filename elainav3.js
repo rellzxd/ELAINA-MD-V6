80f27b0159a08f2bd3412b3996e0e24d2391b764
KamikazakiThouta <ravensvenix@gmail.com>
2022-10-31 02:05:17 +0800

    Add files via upload

--- elainav3.js
+++ elainav3.js
@@ -8167,8 +8167,8 @@ break
   case 'tktoknowm':
 if (isBan) return reply(mess.ban)	 			
 if (isBanChat) return reply(mess.banChat)
-           if (!q) reply(`Example tktoknowm https://vt.tiktok.com/ZSJE2ffo4`)
-			   let urlbngst = await axios.get(`https://api.lolhuman.xyz/api/tiktokwm?apikey=${lolkey}&url=${!q}`)
+           if (!text) reply(`Example tktoknowm https://vt.tiktok.com/ZSJE2ffo4`)
+			   let urlbngst = await axios.get(`https://api.lolhuman.xyz/api/tiktokwm?apikey=${lolkey}&url=${text}`)
 			   let urlbau = await urlbngst(media)
 		   XeonBotInc.sendMessage(from, { video: { url: urlbau.result }, caption: `TikTok Downloader No Watermark !}` }, { quoted: m }).catch(err => console.error('error:' + err));
          break
