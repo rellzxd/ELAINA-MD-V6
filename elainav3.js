78489177795685ce9c842113490c4b28df1cbf37
Xylaa <ravensvenix@gmail.com>
2022-10-31 14:25:26 +0800

    Add files via upload

--- elainav3.js
+++ elainav3.js
@@ -8143,7 +8143,7 @@ if (isBanChat) return reply(mess.banChat)
 			   reply ('Sebentar Kak, lagi loading <3')
 		       linkurl = `https://api.lolhuman.xyz/api/tiktokwm?apikey=${lolkey}&url=${body.slice(11)}`
 			   tiktokk = await fetch(linkurl)
-			   tiktokk2 = await fetchJsonson(tiktokk)
+			   tiktokk2 = await fetchJson(tiktokk)
 		   XeonBotInc.sendMessage(m.chat, { video: { url: tiktokk2 }, caption: `TikTok Download NoWM Selesai!` }, { quoted: m })
 	}
          break
