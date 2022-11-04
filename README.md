ba9ebf1fa21278af581075e212c0f2486bf373bb
RavensVenix <ravensvenix@gmail.com>
2022-11-04 19:01:06 +0800

    v4

---
+++ README.md
@@ -0,0 +1,125 @@
+
+
+<h1 align="center">
+𝑬𝒍𝒂𝒊𝒏𝒂𝑽3 - 𝑿𝒚𝒔𝑺𝒚𝒔𝒕𝒆𝒎<br></h1>
+<p align="center">
+<img src="https://telegra.ph/file/df0edc1680a71b37145af.jpg" />
+</p>
+
+<p align="center">
+𝑬𝒍𝒂𝒊𝒏𝒂𝑽3 - 𝑿𝒚𝒔𝑺𝒚𝒔𝒕𝒆𝒎 | Original Base From <a href="https://github.com/DGXeon" target="_blank">@DGXeon</a> using <a href="https://github.com/adiwajshing/Baileys" target="_blank">Baileys</a> and <a href="https://github.com/nodejs" target="_blank">Nodejs</a>. Dont forget to give a star bro.
+</p>
+
+<p align="center">
+<a href="https://youtu.be/WiIqCdiDjFo"><img title="Size" src="https://img.shields.io/badge/Tutorial-Video-green"></a>
+</p>
+
+------
+
+# ```Bot Info```
+<p align="center">
+<a href="https://github.com/RavensVenix/"><img title="Open Source" src="https://img.shields.io/badge/Author-RavensVenix-red"></a>
+<a href="https://github.com/RavensVenix/elainav3-xyssystem/graphs/commit-activity"><img height="20" src="https://img.shields.io/badge/Maintained-Idk%3F-yellow"></a>&nbsp;&nbsp;
+</p>
+<p align='center'>
+    - CHANGELOG -
+    𝗩𝟯 - 𝗫𝘆𝘀𝗦𝘆𝘀𝘁𝗲𝗺 🪷
+😔 - 𝗥𝗜𝗣 𝗡𝗦𝗙𝗪 𝗙𝗘𝗔𝗧𝗨𝗥𝗘𝗦
+❄️ - 𝗔𝗱𝗱𝗲𝗱 𝗥𝗣𝗚 𝗠𝗲𝗻𝘂 ( 𝗕𝗘𝗧𝗔 )
+🌕 - 𝗔 𝗹𝗼𝘁 𝗼𝗳 𝗳𝘂𝗰𝗸𝗶𝗻𝗴 𝗯𝘂𝗴𝘀 𝗳𝗶𝘅𝗲𝗱
+🧃 - 𝗔𝗱𝗱𝗲𝗱 𝗠𝗼𝗿𝗲 𝗦𝗙𝗪 𝗙𝗲𝗮𝘁𝘂𝗿𝗲𝘀 ( 𝗨𝗣𝗗𝗔𝗧𝗘❗ 𝗕𝗥𝗢𝗞𝗘𝗡 𝗙𝗘𝗔𝗧𝗨𝗥𝗘𝗦 𝗟𝗢𝗟 )
+🔰 - 𝗔𝗱𝗱𝗲𝗱 𝗧𝗶𝗰𝘁𝗮𝗰𝘁𝗼𝗲, 𝘀𝘂𝗶𝘁, 𝘀𝘂𝗶𝘁𝗽𝘃𝗽, 𝗸𝘂𝗶𝘀𝗺𝗮𝘁𝗵, 𝗺𝗮𝘁𝗵, 𝗴𝘂𝗲𝘀𝘀 𝗽𝗶𝗰𝘁𝘂𝗿𝗲/𝘀𝗼𝗻𝗴
+🔯 - 𝗔𝗹𝗹 𝗳𝗲𝗮𝘁𝘂𝗿𝗲𝘀 𝘁𝗵𝗮𝘁 𝘂𝘀𝗶𝗻𝗴 𝗯𝘂𝘁𝘁𝗼𝗻𝘀 𝘄𝗶𝗹𝗹 𝗯𝗿𝗼𝗸𝗲𝗻𝘀.
+    </p>
+
+-------
+
+## ```Let's be friends !```
+<p align="center">
+<a href="https://wa.me/6281338302495"><img src="https://img.shields.io/badge/-CONTACT%206XZY-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" />
+<a href="https://chat.whatsapp.com/K0eLuHKuOYi3DcaUDb1JcK"><img src="https://img.shields.io/badge/Join Official GC-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" />
+<a href="t.me/vmxone"><img src="https://img.shields.io/badge/Telegram-195EFF?style=for-the-badge&logo=telegram&logoColor=ff000000&link=https://www.youtube.com/c/BOTINDO" /><br>
+</p>
+
+## ```Donate Me```
+
+- [`Saweria.co`](https://saweria.co/6xzy)
+
+<p align="left">
+Donate if you want :)
+</p>
+
+## ```Bot Support Groups```
+
+- [`BOT SUPPORT GROUP`](https://chat.whatsapp.com/K0eLuHKuOYi3DcaUDb1JcK)
+
+# Setup For Deployment 👇
+
+- [FORK THIS REPO & GIVE A STARS](https://github.com/RavensVenix/elainav3-xyssystem/fork)
+
+## `Scan QR Code For Session`
+[![Cheems Bot](https://repl.it/badge/github/quiec/whatsasena)](https://replit.com/@DGXeon/Cheems-Bot-Multi-Device-Qr-Code-Generator?output%20only=1&lite=1#index.js)
+
+## `SETTINGS`
+
+- [CHANGE OWNER NUMBER VCARD](https://github.com/RavensVenix/elainav3-xyssystem/blob/master/settings.js#L58)
+- [CHANGE OWNER NUMBER MENU](https://github.com/RavensVenix/elainav3-xyssystem/blob/master/settings.js#L65)
+- [CHANGE OWNER NUMBER TAG](https://github.com/RavensVenix/elainav3-xyssystem/blob/master/settings.js#L66)
+- [CHANGE OWNER NAME](https://github.com/RavensVenix/elainav3-xyssystem/blob/master/settings.js#L59)
+- [CHANGE BOT NAME](https://github.com/RavensVenix/elainav3-xyssystem/blob/master/settings.js#L67)
+
+## ` BUILDPACKS`
+
+```
+https://github.com/jonathanong/heroku-buildpack-ffmpeg-latest
+https://github.com/clhuang/heroku-buildpack-webp-binaries.git
+```
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/RavensVenix/elainav3-xyssystem/)
+
+# Install Manually 👇
+## `Requirements`
+* [Node.js](https://nodejs.org/en/)
+* [Git](https://git-scm.com/downloads)
+* [FFmpeg](https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2020-12-08-13-03/ffmpeg-n4.3.1-26-gca55240b8c-win64-gpl-4.3.zip)
+* [Libwebp](https://developers.google.com/speed/webp/download)
+* Any text editor
+## `Clone Repo & Installation dependencies`
+```bash
+git clone https://github.com/RavensVenix/elainav3-xyssystem.git
+cd elainav3-xyssystem
+
+npm start
+```
+## `For Termux/Ssh/Ubuntu`
+```bash
+apt update
+apt upgrade
+pkg update && pkg upgrade
+pkg install bash
+pkg install libwebp
+pkg install git -y
+pkg install nodejs -y 
+pkg install ffmpeg -y 
+pkg install wget
+pkg install imagemagick -y
+git clone https://github.com/RavensVenix/elainav3-xyssystem
+cd elainav3-xyssystem
+npm start
+```
+## `For VPS`
+```bash
+apt install nodejs 
+apt install git 
+apt apt install ffmpeg 
+apt apt install libwebp 
+apt apt install imagrmagick
+apt install bash
+git clone https://github.com/RavensVenix/elainav3-xyssystem
+cd elainav3-xyssystem
+npm start
+```
+## `For 24/7 Activation (Termux)`
+```bash
+npm i -g pm2 && pm2 start elaina.js && pm2 save && pm2 logs
+```
---
+++ settings.js
@@ -0,0 +1,155 @@
+//════════════════════════════//
+//If you want to recode, reupload
+//or copy the codes/script,
+//pls give credit
+//no credit? i will take action immediately
+//© 2022 Xeon Bot Inc. Cheems Bot MD
+//Thank you to Lord Buddha, Family and Myself
+//════════════════════════════//
+//recode kar ke youtube pe upload kar rhe hai ya
+//codes copy kar ke apne script me dal rhe
+//hai to, description me xeon ka yt channel
+// ka link paste kr dena as a cradit or github 
+//repo me bhi tag kardena baki jo
+//bhi karna hai apki marzi, thank you!🦄
+//════════════════════════════//
+//If you recode and uploading on your channel
+//or copy pasting the codes in ur script, 
+//i give permission to do as long as you
+//put Xeons youtube channel link in the video
+//description and tag me on githuh repo, 
+//thank you🦄
+//════════════════════════════//
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+//Api Website\\
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+//Api Key\\
+global.APIKeys = {
+	'https://zenzapis.xyz': 'edde6d40d3bc',
+}
+
+
+//aumto functioner
+global.autoTyping = true //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = true //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = true //auto reading in gc (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
+global.available = true //auto available (true to on, false to off)
+
+//semt menu variants
+global.vidmenu = fs.readFileSync("./XeonMedia/theme/jojo.mp4") //gif and video menu
+
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+//owmner v card
+lolkey = '2f73fe5296d00f4fb394a468'
+frkey = 'kZgLGZbX'
+apidanz = '92b0a75k6f'
+global.vcardowner = ['6281338302495'], ['6285338440313'] //number
+global.allpay = "081338302495" // nomor pertama / first number donate
+global.allpay2 = "085338440313" // nomor ke dua /second number donate
+global.ownername = "❣︎xylaa✞ psatir bet coeg" //owner
+global.email = "📧 vynx14u@gmail.com" //email
+global.website = "⚙️ Github : RavensVenix" //github name
+global.location = "🌍 Isekai, Taman Hiburan Para Loli, Pemandian Umum Wanita" //location
+global.roblox = "👾 Roblox : @JustRyyXD" //roblox
+global.ig = "👑 Instagram : @ravensvenix" //insta
+global.tg = "💙 Telegram : @vmxone" //telegram
+
+//bot bomdy 
+global.owner = ['6281338302495'], ['6285338440313']
+global.ownertag = '6281338302495' //ur tag number
+global.botname = '❣︎𝟔𝐱𝐑𝐞𝐯𝐞𝐠𝐞𝐧𝐜𝐲✞' //ur bot name
+global.linkz = "nhentai.net" //your theme url which will be displayed on whatsapp
+global.websitex = "http://nyxworldx.blogspot.com/" //ur website to be displayed
+global.botscript = 'https://github.com/RavensVenix/elainav3-xyssystem' //script link
+global.reactmoji = "🦋" //ur menu react emoji
+global.themeemoji = "🦋" //ur theme emoji
+global.packname = "❣︎𝟔𝐱𝐑𝐞𝐯𝐞𝐠𝐞𝐧𝐜𝐲✞\n\n\n\n\n\n\n\n\n\n\n• StickerPack " //ur sticker watermark packname
+global.author = "❣︎𝟔𝐱𝐑𝐞𝐯𝐞𝐠𝐞𝐧𝐜𝐲✞" //ur sticker watermark author
+
+//Bot theme media
+global.thum = fs.readFileSync("./XeonMedia/theme/thumb.jpeg") //ur thumb pic
+global.log0 = fs.readFileSync("./XeonMedia/theme/logo.jpeg") //ur logo pic
+global.err4r = fs.readFileSync("./XeonMedia/theme/thumb.jpeg") //ur error pic
+global.thumb = fs.readFileSync("./XeonMedia/theme/thumb.jpeg") //ur thumb pic
+
+//damtabase
+global.premium = ['6281338302495'] //ur premium numbers
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+//omther
+global.sessionName = 'session'
+global.antitags = true
+global.prefa = ['','!','.','?','#','-']
+global.sp = '⭔'
+global.mess = {
+    success: 'Done ✓',
+    admin: 'This Feature Is Only For Admin!',
+    botAdmin: 'Bot Must Be Admin First!',
+    owner: 'This Feature Is Only For Owner!',
+    group: 'Feature Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'This Feature Is Only For Bot',
+    wait: 'In process...',
+    linkm: 'Where is the link?',
+    error: 'Error!!',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
+    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
+    banChat: 'The bot was banned in this group, please contact the owner to unban'
+}
+    global.limitawal = {
+    premium: "Infinity",
+    free: 12,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./XeonMedia/theme/thumb.jpeg')
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
